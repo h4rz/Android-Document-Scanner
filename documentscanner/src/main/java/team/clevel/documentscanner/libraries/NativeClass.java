cca5219ced8023cb39b110111f78441d3278250a
Harsh Rajgor <harshrajgor996@gmail.com>
2020-04-15 12:10:43 +0530

    rectangle detection on white on white image

--- documentscanner/src/main/java/team/clevel/documentscanner/libraries/NativeClass.java
+++ documentscanner/src/main/java/team/clevel/documentscanner/libraries/NativeClass.java
@@ -11,21 +11,25 @@ package team.clevel.documentscanner.libraries;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.util.Log;
 
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
+import org.opencv.core.KeyPoint;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfKeyPoint;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
+import org.opencv.features2d.FeatureDetector;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -138,6 +142,139 @@ public class NativeClass {
             }
         }
 
+        if (rectangles.size() != 0)
+            return rectangles;
+        return getRectanglesFromTextDetection(src);
+    }
+
+    private List<MatOfPoint2f> getRectanglesFromTextDetection(Mat src) {
+        Log.i("***** NATIVE CLASS - ", "Detecting from text");
+        Scalar CONTOUR_COLOR = new Scalar(255, 0, 0, 255);
+        Mat mGrey = new Mat(src.size(), CvType.CV_8UC4);
+        Imgproc.cvtColor(src, mGrey, Imgproc.COLOR_RGBA2GRAY);
+        Mat mRgba = new Mat(src.size(), CvType.CV_8UC4);
+        mRgba = src;
+        List<MatOfPoint2f> rectangles = new ArrayList<>();
+        MatOfKeyPoint keypoint = new MatOfKeyPoint();
+        List<KeyPoint> listpoint;
+        KeyPoint kpoint;
+        Mat mask = Mat.zeros(mGrey.size(), CvType.CV_8UC1);
+        int rectanx1;
+        int rectany1;
+        int rectanx2;
+        int rectany2;
+        int imgsize = mGrey.height() * mGrey.width();
+        Scalar zeos = new Scalar(0, 0, 0);
+
+        List<MatOfPoint> contour2 = new ArrayList<MatOfPoint>();
+        Mat kernel = new Mat(1, 50, CvType.CV_8UC1, Scalar.all(255));
+        Mat morbyte = new Mat();
+        Mat hierarchy = new Mat();
+
+        Rect rectan3;
+
+        FeatureDetector detector = FeatureDetector
+                .create(FeatureDetector.MSER);
+        detector.detect(mGrey, keypoint);
+        listpoint = keypoint.toList();
+
+        for (int ind = 0; ind < listpoint.size(); ind++) {
+            kpoint = listpoint.get(ind);
+            rectanx1 = (int) (kpoint.pt.x - 0.5 * kpoint.size);
+            rectany1 = (int) (kpoint.pt.y - 0.5 * kpoint.size);
+            rectanx2 = (int) (kpoint.size);
+            rectany2 = (int) (kpoint.size);
+            if (rectanx1 <= 0)
+                rectanx1 = 1;
+            if (rectany1 <= 0)
+                rectany1 = 1;
+            if ((rectanx1 + rectanx2) > mGrey.width())
+                rectanx2 = mGrey.width() - rectanx1;
+            if ((rectany1 + rectany2) > mGrey.height())
+                rectany2 = mGrey.height() - rectany1;
+            Rect rectant = new Rect(rectanx1, rectany1, rectanx2, rectany2);
+            try {
+                Mat roi = new Mat(mask, rectant);
+                roi.setTo(CONTOUR_COLOR);
+            } catch (Exception ex) {
+                Log.d("mylog", "mat roi error " + ex.getMessage());
+            }
+        }
+
+        Imgproc.morphologyEx(mask, morbyte, Imgproc.MORPH_DILATE, kernel);
+        Imgproc.findContours(morbyte, contour2, hierarchy,
+                Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+        List<MatOfPoint> usefulContours = new ArrayList<MatOfPoint>();
+        for (int ind = 0; ind < contour2.size(); ind++) {
+            //rectan3 = Imgproc.boundingRect(contour2.get(ind));
+            rectan3 = Imgproc.boundingRect(contour2.get(ind));
+            if (rectan3.area() > 0.5 * imgsize || rectan3.area() < 100
+                    || rectan3.width / rectan3.height < 2) {
+                Mat roi = new Mat(morbyte, rectan3);
+                roi.setTo(zeos);
+
+            } else
+                usefulContours.add(contour2.get(ind));
+        }
+
+        Rect firstRect = new Rect();
+        Rect lastRect = new Rect();
+
+
+        for (int ind = 0; ind < usefulContours.size(); ind++) {
+            rectan3 = Imgproc.boundingRect(contour2.get(ind));
+            if (ind == 0) {
+                lastRect = rectan3;
+            } else if (ind == usefulContours.size() - 1) {
+                firstRect = rectan3;
+            }
+            //Imgproc.rectangle(mRgba, rectan3.br(), rectan3.tl(), CONTOUR_COLOR);
+        }
+
+        if (firstRect.width > lastRect.width)
+            lastRect.width = firstRect.width;
+        else
+            firstRect.width = lastRect.width;
+
+        Rect newRectangle = new Rect(lastRect.br(), firstRect.tl());
+
+        Imgproc.rectangle(mRgba, newRectangle.br(), newRectangle.tl(), CONTOUR_COLOR);
+
+        List<Point> points = new ArrayList<>();
+        int minX = newRectangle.x;
+        int minY = newRectangle.y;
+        int maxX = newRectangle.x + newRectangle.width;
+        int maxY = newRectangle.y + newRectangle.height;
+        if (maxX > mRgba.width())
+            maxX = mRgba.width();
+        if (maxY > mRgba.height())
+            maxX = mRgba.height();
+        if (minX < 0)
+            minX = 0;
+        if (minY < 0)
+            minY = 0;
+        Point topLeft = new Point(minX, minY);
+        Point topRight = new Point(maxX, minY);
+        Point bottomLeft = new Point(maxX, maxY);
+        Point bottomRight = new Point(minX, maxY);
+        points.add(topLeft);
+        points.add(topRight);
+        points.add(bottomLeft);
+        points.add(bottomRight);
+
+        MatOfPoint temp = new MatOfPoint();
+        temp.fromList(points);
+
+        MatOfPoint2f contourFloat = MathUtils.toMatOfPointFloat(temp);
+        double arcLen = Imgproc.arcLength(contourFloat, true) * 0.02;
+
+        // Approximate polygonal curves.
+        MatOfPoint2f approx = new MatOfPoint2f();
+        Imgproc.approxPolyDP(contourFloat, approx, arcLen, true);
+
+        //if (isRectangle(approx, srcArea)) {
+        rectangles.add(approx);
+        //}
         return rectangles;
     }
 
